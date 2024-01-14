24b996c00f762b6107e17773e65bd61ccbd53c3e
Gabriel Azevedo <goazevedo@icloud.com>
2024-01-14 05:40:06 -0300

    Initial Commit

---
+++ FirstEngine/FirstEngineApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstEngineApp.swift
+//  FirstEngine
+//
+//  Created by Gabriel Azevedo on 14/01/24.
+//
+
+import SwiftUI
+
+@main
+struct FirstEngineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
